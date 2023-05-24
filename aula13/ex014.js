527341ea9dcc2f6736cc2bb307fbac9a4dcd6039
laramaglia <130146747+laramaglia@users.noreply.github.com>
2023-05-24 18:45:49 -0300

    exercicio aula 13 de repetições

---
+++ aula13/ex014.js
@@ -0,0 +1,25 @@
+// Repetições
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/*
+var c = 1 
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // significa que: c = c + 1
+}
+*/
+
+
+
+
+
+
+
+
+
